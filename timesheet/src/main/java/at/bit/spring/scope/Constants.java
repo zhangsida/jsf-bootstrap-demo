15f4a950eeacc85837d8f28af7beadeedccd92e7
Philipp Bayer <philipp.bayer@bridging-it.de>
2014-02-17 13:39:10 +0100

    new stuff

--- timesheet/src/main/java/at/bit/spring/scope/Constants.java
+++ timesheet/src/main/java/at/bit/spring/scope/Constants.java
@@ -10,14 +10,21 @@ public class Constants {
 	public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormat.forPattern("dd/MM/yyyy");
 	public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormat.forPattern("HH:mm");
 
-	private Integer[] hours = { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4 };
+	private static final String NAV_EVENTS_OVERVIEW = "eventsOverviewView.xhtml";
+	private static final String NAV_DAYVIEW = "dayView.xhtml";
+
+	private static final Integer[] HOURS = { 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0, 1, 2, 3, 4 };
 
 	public Integer[] getHours() {
-		return hours;
+		return HOURS;
 	}
 
-	public void setHours(final Integer[] hours) {
-		this.hours = hours;
+	public String getNavEventsOverview() {
+		return NAV_EVENTS_OVERVIEW;
+	}
+
+	public String getNavDayview() {
+		return NAV_DAYVIEW;
 	}
 
 }
