66eea1b32567ab64face54d792e9897fea858c41
Philipp Bayer <philipp.bayer@bridging-it.de>
2014-02-20 10:52:22 +0100

    Javadoc

--- timesheet/src/main/java/at/bit/controller/PaginationController.java
+++ timesheet/src/main/java/at/bit/controller/PaginationController.java
@@ -2,6 +2,12 @@ package at.bit.controller;
 
 import java.util.List;
 
+/**
+ * Interface class to be used together with the pagination composite component.
+ * 
+ * @author pbayer
+ * @param <T>
+ */
 public interface PaginationController<T> {
 
 	void prevPage();
