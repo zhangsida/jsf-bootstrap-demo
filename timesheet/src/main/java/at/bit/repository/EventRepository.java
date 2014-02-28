1486eb7d46cb8e0ae65ad397799ee017e481f526
Philipp Bayer <philipp.bayer@bridging-it.de>
2014-02-28 13:08:47 +0100

    Javadoc

--- timesheet/src/main/java/at/bit/repository/EventRepository.java
+++ timesheet/src/main/java/at/bit/repository/EventRepository.java
@@ -9,6 +9,14 @@ import at.bit.model.Event;
 
 public interface EventRepository extends GraphRepository<Event> {
 
+	/**
+	 * Custom pagination query, since we want to order by date which isnt a
+	 * direct property of Events
+	 * 
+	 * @param page
+	 *            the page to fetch
+	 * @return the current page
+	 */
 	@Query(value = "START event=node:__types__(className='at.bit.model.Event') MATCH  event<-[:eventsToDay]-day RETURN event ORDER BY day.date DESC", countQuery = "START event=node:__types__(className='at.bit.model.Event') RETURN count(event)")
 	public Page<Event> fetchOrderedPage(Pageable page);
 
