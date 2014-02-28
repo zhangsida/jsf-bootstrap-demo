fb6d46b1f35e07f8ac96d718db2f95df890f7363
Philipp Bayer <philipp.bayer@bridging-it.de>
2014-02-28 13:01:31 +0100

    Unittests

--- timesheet/src/test/java/at/bit/model/EventTest.java
+++ timesheet/src/test/java/at/bit/model/EventTest.java
@@ -2,12 +2,43 @@ package at.bit.model;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import javax.validation.ValidationException;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import at.bit.repository.EventRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/testContext.xml")
+@ActiveProfiles("test")
 public class EventTest {
 
 	private final String testdata[][] = { { "15:00", "16:00", "1" }, { "15:00", "16:01", "2" }, { "15:20", "16:20", "2" }, { "15:20", "18:20", "4" },
 			{ "15:20", "16:21", "2" }, { "15:20", "17:21", "3" }, { "15:40", "16:20", "2" }, { "15:40", "17:20", "3" }, { "15:20", "17:40", "3" }, };
+
+	@Autowired
+	private EventRepository eventRepo;
+
+	@Test(expected = ValidationException.class)
+	@Transactional
+	public void testBeanValidationNoName() {
+		Event e = new Event();
+		eventRepo.save(e);
+	}
+
+	@Test()
+	@Transactional
+	public void testBeanValidationOK() {
+		Event e = new Event();
+		e.setName("safdasf");
+		eventRepo.save(e);
+	}
 
 	@Test
 	public void testDurationHours() throws Exception {
